5df16bac8a8300062b33df07c4d37c0cb79e87a5
Vishal Khedkar <www.vishal8094@gmail.com>
2023-06-17 14:56:02 +0530

    Done some minor changes in in files

--- app/src/main/java/com/example/myapplication/Articles.java
+++ app/src/main/java/com/example/myapplication/Articles.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.WindowManager;
 
 public class Articles extends AppCompatActivity {
-    //Hi It's Vishal
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
